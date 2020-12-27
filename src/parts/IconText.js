912712179f726c788b6252bb4581c80ff3e10387
risyad azhary <risyad01@gmail.com>
2020-12-27 22:38:15 +0700

    Create header componentand put to arrange LandingPage

---
+++ src/parts/IconText.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Button from "elements/Button";
+
+// export default function IconText() {
+// 	return (
+// 		<Button className='brand-text-icon' href='' type='link'>
+// 			Libur <span className='text-teal'> an.</span>
+// 		</Button>
+// 	);
+// }
+
+export default function IconText() {
+	return (
+		<Button className='brand-text-icon' href='' type='link'>
+			Libur<span className='text-teal'>an.</span>
+		</Button>
+	);
+}
